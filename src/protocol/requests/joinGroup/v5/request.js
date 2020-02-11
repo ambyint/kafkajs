a76ec6700bfb3c4be6246114cf5ef940ac29b1d1
Ryan Benoit <rdbenoit@me.com>
2020-02-11 12:08:21 -0700

    Add v5 join group protocol
    Signed-off-by: Ryan Benoit <rdbenoit@me.com>

---
+++ src/protocol/requests/joinGroup/v5/request.js
@@ -0,0 +1,43 @@
+const Encoder = require('../../../encoder')
+const { JoinGroup: apiKey } = require('../../apiKeys')
+
+/**
+ * JoinGroup Request (Version: 5) => group_id session_timeout_ms rebalance_timeout_ms member_id group_instance_id protocol_type [protocols]
+ * group_id => STRING
+ * session_timeout_ms => INT32
+ * rebalance_timeout_ms => INT32
+ * member_id => STRING
+ * group_instance_id => NULLABLE_STRING
+ * protocol_type => STRING
+ * protocols => name metadata
+  * name => STRING
+  * metadata => BYTES
+ */
+
+module.exports = ({
+                    groupId,
+                    sessionTimeout,
+                    rebalanceTimeout,
+                    memberId,
+                    groupInstanceId = null,
+                    protocolType,
+                    groupProtocols,
+                  }) => ({
+  apiKey,
+  apiVersion: 5,
+  apiName: 'JoinGroup',
+  encode: async () => {
+    return new Encoder()
+      .writeString(groupId)
+      .writeInt32(sessionTimeout)
+      .writeInt32(rebalanceTimeout)
+      .writeString(memberId)
+      .writeString(groupInstanceId)
+      .writeString(protocolType)
+      .writeArray(groupProtocols.map(encodeGroupProtocols))
+  },
+})
+
+const encodeGroupProtocols = ({ name, metadata = Buffer.alloc(0) }) => {
+  return new Encoder().writeString(name).writeBytes(metadata)
+}
---
+++ src/protocol/requests/joinGroup/v5/response.js
@@ -0,0 +1,44 @@
+const Decoder = require('../../../decoder')
+const { failIfVersionNotSupported } = require('../../../error')
+const { parse: parseV0 } = require('../v0/response')
+
+/**
+ * JoinGroup Response (Version: 5) => throttle_time_ms error_code generation_id protocol_name leader member_id [members]
+ * throttle_time_ms => INT32
+ * error_code => INT16
+ * generation_id => INT32
+ * protocol_name => STRING
+ * leader => STRING
+ * member_id => STRING
+ * members => member_id group_instance_id metadata
+ * member_id => STRING
+ * group_instance_id => NULLABLE_STRING
+ * metadata => BYTES
+ */
+
+const decode = async rawData => {
+  const decoder = new Decoder(rawData)
+  const throttleTime = decoder.readInt32()
+  const errorCode = decoder.readInt16()
+
+  failIfVersionNotSupported(errorCode)
+
+  return {
+    throttleTime,
+    errorCode,
+    generationId: decoder.readInt32(),
+    groupProtocol: decoder.readString(),
+    leaderId: decoder.readString(),
+    memberId: decoder.readString(),
+    members: decoder.readArray(decoder => ({
+      memberId: decoder.readString(),
+      groupInstanceId: decoder.readString(),
+      memberMetadata: decoder.readBytes(),
+    })),
+  }
+}
+
+module.exports = {
+  decode,
+  parse: parseV0,
+}
